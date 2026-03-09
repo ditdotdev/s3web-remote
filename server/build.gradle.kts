23436a34a1ea4ebf179ce1d25d2b82c245d378ea
Robert Eric Reeves <robertericreeves@gmail.com>
2026-03-09 11:26:34 -0500

    Update dependencies to 1.7.0

--- server/build.gradle.kts
+++ server/build.gradle.kts
@@ -21,7 +21,7 @@ repositories {
 
 dependencies {
 	implementation(kotlin("stdlib"))
-	implementation("com.datadatdat:remote-sdk:1.6.1")
+	implementation("com.datadatdat:remote-sdk:1.7.0")
 	implementation("com.google.code.gson:gson:2.13.2")
 	implementation("com.squareup.okhttp3:okhttp:5.3.2")
 	testImplementation("io.kotlintest:kotlintest-runner-junit5:3.4.2")
